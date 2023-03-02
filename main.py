ba7e01c1e0d6b9d56ce8c2ee61e94874bf38e501
Mingxun Wang <mwang87@gmail.com>
2023-03-02 09:55:56 -0800

    updating output

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 from IPython.display import SVG
 
-def main(s1, s2, smiles, output, show_img=True):
+def sitelocalize(spec1, spec2, smiles1, show_img=True):
 
-    molMol = Chem.MolFromSmiles(smiles)
+    molMol = Chem.MolFromSmiles(smiles1)
 
-    modsite = SiteLocator.SiteLocator(s1, s2, molMol)
+    modsite = SiteLocator.SiteLocator(spec1, spec2, molMol)
     scores_unshifted, scores_shifted = modsite.calculate_score()
     scores = modsite.distance_score(scores_unshifted, scores_shifted)
     if (max(scores.values()) > 0):
@@ -42,7 +42,16 @@ def main(s1, s2, smiles, output, show_img=True):
     result['# shifted peaks'] = len(modsite.shifted)
     result['image'] = d2d.GetDrawingText()
 
-    visualizer.table_to_xlsx(result, output)
+    return result
+
+def main(s1, s2, smiles, output_file, output_image_folder, show_img=True):
+    result = sitelocalize(s1, s2, smiles, show_img=show_img)
+
+    if output_file.endswith('.xlsx'):
+        visualizer.table_to_xlsx(result, output_file)
+    elif output_file.endswith('.tsv'):
+        # writing out the tsv file
+        visualizer.table_to_tsv(result, output_file, output_image_folder)
 
 
 if __name__ == '__main__':
@@ -51,8 +60,9 @@ if __name__ == '__main__':
     parser.add_argument('--s1', type=str, default='mzspec:GNPS:TASK-e31d8bbfb26c4c3e8f159f06e37d65bb-spectra/specs_ms.mgf:scan:3189', help='gnps accepted usi for known molecule')
     parser.add_argument('--s2', type=str, default='mzspec:GNPS:TASK-e31d8bbfb26c4c3e8f159f06e37d65bb-spectra/specs_ms.mgf:scan:3399', help='gnps accepted usi for modified molecule')
     parser.add_argument('--smiles', type=str, default='OC(c1c(CCCCC)cc(OC(c2c(OC)cc(OC)cc2CCCCC)=O)cc1OC)=O', help='smiles for the known molecule')
-    parser.add_argument('--output', type=str, default="output.xlsx", help='output file')
+    parser.add_argument('--output_file', type=str, default="output.xlsx", help='output file')
+    parser.add_argument('--output_image_folder', type=str, default=".", help='output img folder')
     parser.add_argument('--show_img', type=bool, default=True, help='show final image')
     args = parser.parse_args()
 
-    main(args.s1, args.s2, args.smiles, args.output)
+    main(args.s1, args.s2, args.smiles, args.output_file, args.output_image_folder)
