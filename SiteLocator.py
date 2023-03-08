e775ba788f52db7594ff416f79625902f300334c
user <user@reza.cs.ucr.edu>
2023-03-08 12:50:08 -0800

    returning stats in accuracy function

--- SiteLocator.py
+++ SiteLocator.py
@@ -32,13 +32,17 @@ class SiteLocator():
         self.shifted = shifted
         self.unshifted = unshifted
     
-    def calculate_score(self):
+    def calculate_score(self, peak_presence_only = False):
         self.appearance_shifted = {i: [] for i in range(0, self.molMol.GetNumAtoms())}
         self.appearance_unshifted = {i: [] for i in range(0, self.molMol.GetNumAtoms())}
 
         shifted, unshifted = utils.separateShifted(self.matchedPeaks, self.molPeaks, self.modifPeaks)
 
+        atom_peak_presence_shifted = {i: 0 for i in range(0, self.molMol.GetNumAtoms())}
+        atom_peak_presence_unshifted = {i: 0 for i in range(0, self.molMol.GetNumAtoms())}
+
         for peak in unshifted:
+            atom_presence = set()
             possiblities = self.fragments.find_fragments(self.molPeaks[peak[0]][0], 0.1, 0.98, 0.01)
             for possibility in possiblities:
                 smiles = self.fragments.get_fragment_info(possibility[0], 0)[3]
@@ -46,9 +50,14 @@ class SiteLocator():
                 hitAtoms, hitBonds = utils.getHitAtomsAndBonds(self.molMol, substructure)
                 for atomSet in hitAtoms:
                     for atom in atomSet:
+                        atom_presence.add(atom)
                         self.appearance_unshifted[atom].append(possibility[0])
             
+            for atom in atom_presence:
+                atom_peak_presence_unshifted[atom] += 1
+            
         for peak in shifted:
+            atom_presence = set()
             possiblities = self.fragments.find_fragments(self.molPeaks[peak[0]][0], 0.1, 0.98, 0.5)
             for possibility in possiblities:
                 smiles = self.fragments.get_fragment_info(possibility[0], 0)[3]
@@ -56,19 +65,30 @@ class SiteLocator():
                 hitAtoms, hitBonds = utils.getHitAtomsAndBonds(self.molMol, substructure)
                 for atomSet in hitAtoms:
                     for atom in atomSet:
+                        atom_presence.add(atom)
                         self.appearance_shifted[atom].append(possibility[0])
+            
+            for atom in atom_presence:
+                atom_peak_presence_shifted[atom] += 1
         
         scores_unshifted = []
         scores_shifted = []
         for i in range(0, self.molMol.GetNumAtoms()):
-            scores_unshifted.append(len(self.appearance_unshifted[i]))
-            scores_shifted.append(len(self.appearance_shifted[i]))
+            if peak_presence_only:
+                scores_unshifted.append(atom_peak_presence_unshifted[i])
+                scores_shifted.append(atom_peak_presence_shifted[i])
+            else:
+                scores_unshifted.append(len(self.appearance_unshifted[i]))
+                scores_shifted.append(len(self.appearance_shifted[i]))
         return scores_unshifted, scores_shifted
     
-    def distance_score(self, scores_unshifted, scores_shifted):
+    def distance_score(self, scores_unshifted, scores_shifted, combine = True):
         scores = {}
         for i in range(0, self.molMol.GetNumAtoms()):
-            scores[i] = scores_shifted[i] #- scores_unshifted[i]
+            if combine:
+                scores[i] = scores_shifted[i] - scores_unshifted[i]
+            else:
+                scores[i] = scores_shifted[i]
         
         ## add min of scores to all in scores
         minScore = min(scores.values())
@@ -76,24 +96,37 @@ class SiteLocator():
             scores[i] += max(0, -minScore)
         return scores
     
-    def accuracy_score(self, modificationSiteIdx):
+    def accuracy_score(self, modificationSiteIdx, peak_presence_only = False, combine = True, getClosestMaxAtomDistance = False, getCountMax = False, getIsMax = False):
         
-        scores_unshifted, scores_shifted = self.calculate_score()
-        scores = self.distance_score( scores_unshifted, scores_shifted)
+        scores_unshifted, scores_shifted = self.calculate_score(peak_presence_only)
+        scores = self.distance_score( scores_unshifted, scores_shifted, combine)
 
         Max = max(scores.values())
         if Max == 0:
             return 0
-        if scores[modificationSiteIdx] == Max:
-            return 1
+        
+        closestMaxAtom = 0
+        distances = 0
+        count = 0
+        for i in range(0, self.molMol.GetNumAtoms()):
+            if scores[i] == Max:
+                count += 1
+                distances += (self.distances[modificationSiteIdx][i]/np.amax(self.distances))
+                if self.distances[modificationSiteIdx][i] < self.distances[modificationSiteIdx][closestMaxAtom]:
+                    closestMaxAtom = i
+        
+        if getCountMax or getIsMax or getClosestMaxAtomDistance:
+            res = {'score': 1 - (distances/count)}
+            if getCountMax:
+                res['count'] = count
+            if getIsMax:
+                res['isMax'] = (scores[modificationSiteIdx] == Max)
+            if getClosestMaxAtomDistance:
+                res['closestMaxAtomDistance'] = closestMaxAtom
         else:
-            distances = 0
-            count = 0
-            for i in range(0, self.molMol.GetNumAtoms()):
-                if scores[i] == Max:
-                    count += 1
-                    distances += (self.distances[modificationSiteIdx][i]/np.amax(self.distances))
-            return 1 - (distances / count)
+            res = 1 - (distances/count)
+        
+        return res
     
     def get_peaks_per_atom(self, atomIdx):
         return self.appearance_shifted[atomIdx], self.appearance_unshifted[atomIdx]
