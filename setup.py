3d6a9585481f10422eda2237c616aeba9b9fc184
mshahneh <mzare008@ucr.edu>
2024-05-08 13:41:14 -0700

    minor improvements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ModiFinder',
-    version='1.03',
+    version='1.04',
     packages=find_packages(),
     install_requires=[
         'cairosvg',
